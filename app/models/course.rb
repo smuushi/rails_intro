24aba3fe48f5532c74cf4bae7214721f97690310
smuushi <mishih780@gmail.com>
2023-02-09 14:32:18 -0500

    enrollment

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,24 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :bigint           not null, primary key
+#  name          :string           not null
+#  prereq_id     :bigint
+#  instructor_id :bigint           not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
 class Course < ApplicationRecord
+    validates :name, presence: true
+    validates :instructor_id, presence: true
+
+    has_many(
+        :enrollments,
+        class_name: :Enrollment,
+        primary_key: :id,
+        foreign_key: :course_id
+    )
+
+
 end
--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,2 +1,29 @@
+# == Schema Information
+#
+# Table name: enrollments
+#
+#  id         :bigint           not null, primary key
+#  course_id  :bigint           not null
+#  student_id :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Enrollment < ApplicationRecord
+    validates :course_id, presence: true
+    validates :student_id, presence: true
+
+    belongs_to(
+        :student,
+        class_name: :User, 
+        primary_key: :id,
+        foreign_key: :student_id
+    )
+
+    belongs_to(
+        :course,
+        class_name: :Course,
+        primary_key: :id, 
+        foreign_key: :course_id
+    )
+
 end
